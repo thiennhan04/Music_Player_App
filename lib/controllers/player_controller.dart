0b551b9e4e0654613605a698622c03b252ecbbe6
nguyenthiennhan0402 <96nguyenthiennhan@gmail.com>
2024-01-29 01:02:56 +0700

    play music feature

--- lib/controllers/player_controller.dart
+++ lib/controllers/player_controller.dart
@@ -1,14 +1,30 @@
 
 import 'package:get/get.dart';
+import 'package:just_audio/just_audio.dart';
 import 'package:on_audio_query/on_audio_query.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 class PlayerController extends GetxController{
   final audioQuery = new OnAudioQuery();
+  final audioPlayer = AudioPlayer();
+  var playIndex = 0.obs;
+  var isPlaying = false.obs;
   @override
   void onInit() {
     super.onInit();
     checkPermission();
+  }
+  playSong(String? uri, index){
+    playIndex.value = index;
+    try{
+        audioPlayer.setAudioSource(
+          AudioSource.uri(Uri.parse(uri!)),
+        );
+        audioPlayer.play();
+        isPlaying(true);
+    } on Exception catch (e){
+      print(e.toString());
+    }
   }
   checkPermission() async {
     var perm = await Permission.storage.request();
--- lib/views/home.dart
+++ lib/views/home.dart
@@ -35,18 +35,20 @@ class Home extends StatelessWidget {
         ),
         body: FutureBuilder<List<SongModel>>(
           future: controller.audioQuery.querySongs(
-            ignoreCase: true,
-            orderType: OrderType.ASC_OR_SMALLER,
-            sortType: null,
-            uriType: UriType.EXTERNAL
-          ),
-          builder: (BuildContext context, snapshot){
-            if(snapshot.data == null){
+              ignoreCase: true,
+              orderType: OrderType.ASC_OR_SMALLER,
+              sortType: null,
+              uriType: UriType.EXTERNAL),
+          builder: (BuildContext context, snapshot) {
+            if (snapshot.data == null) {
               print(snapshot.data);
               return Center(child: CircularProgressIndicator());
-            } else if(snapshot.data!.isEmpty){
+            } else if (snapshot.data!.isEmpty) {
               return Center(
-                child: Text("No song found", style: ourStyle(),),
+                child: Text(
+                  "No song found",
+                  style: ourStyle(),
+                ),
               );
             } else {
               return Padding(
@@ -57,22 +59,42 @@ class Home extends StatelessWidget {
                   itemBuilder: (BuildContext context, int index) {
                     return Container(
                       margin: EdgeInsets.only(bottom: 4),
-
-                      child: ListTile(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(12)
-                        ),
-                        tileColor: bgColor,
-                        title: Text("${snapshot.data![index].displayNameWOExt}", style: ourStyle(
-                            family: bold, size: 15),),
-                        subtitle:Text("${snapshot.data![index].artist}", style: ourStyle(
-                            family: regular, size: 12),),
-                        leading: Icon(Icons.music_note, color: whiteColor, size: 32),
-                        trailing: const Icon(Icons.play_arrow, color: whiteColor, size: 26,),
-
-                      ),
+                      child: Obx(() => ListTile(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(12)),
+                            tileColor: bgColor,
+                            title: Text(
+                              "${snapshot.data![index].displayNameWOExt}",
+                              style: ourStyle(family: bold, size: 15),
+                            ),
+                            subtitle: Text(
+                              "${snapshot.data![index].artist}",
+                              style: ourStyle(family: regular, size: 12),
+                            ),
+                            leading: QueryArtworkWidget(
+                              id: snapshot.data![index].id,
+                              type: ArtworkType.AUDIO,
+                              nullArtworkWidget: const Icon(
+                                Icons.music_note,
+                                color: whiteColor,
+                                size: 32,
+                              ),
+                            ),
+                            trailing: controller.playIndex == index && controller.isPlaying.value
+                                ? const Icon(
+                                    Icons.play_arrow,
+                                    color: whiteColor,
+                                    size: 26,
+                                  )
+                                : null,
+                            onTap: () {
+                              controller.playSong(
+                                  snapshot.data![index].uri, index);
+                            },
+                          )),
                     );
-                  },),
+                  },
+                ),
               );
             }
           },
